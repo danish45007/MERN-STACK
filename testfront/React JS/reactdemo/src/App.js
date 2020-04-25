9398156572362674507f0b7f48f917c8822b97ee
danish45007 <myemail>
2020-04-26 02:36:24 +0530

    First-commit/ FrontEnd Test

---
+++ testfront/React JS/reactdemo/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className = "App">
+      <header className = "App-header">
+        <img src = {logo} className = "App-logo"/>
+        <p>"My name is Danish and i am learning ReactJs"</p>
+      </header>
+    
+    </div>
+  );
+};
+
+
+
+export default App;
