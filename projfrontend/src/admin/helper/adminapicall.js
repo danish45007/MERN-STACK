40012afed6e516ac4f9b84e96c26fa077a3d3d28
danish45007 <myemail>
2020-05-01 02:58:53 +0530

    Fourth Commit Frontend

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -28,6 +28,24 @@ export const getCategories = () => {
     .catch(err => console.log(err));
 };
 
+// delete category
+
+export const deleteCategory = (categoryId, userId, token) => {
+  return fetch(`${API}/category/${categoryId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+
+
 //products calls
 
 //create a product
